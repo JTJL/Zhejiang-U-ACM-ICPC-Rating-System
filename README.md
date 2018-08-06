2bdf359960859c55e2b74905e26f5d6907ca3c71
JTJL <ch.papaw@gmail.com>
2018-08-06 21:14:41 +0800

    fix path

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ This is an easy rating system for Zhejiang University ACM-ICPC team.
 Usage
 ---
 
-1. Add teams into ```team.txt```
-2. For each contest, make a file named ```contest[*].txt```, which starts from 1. In the contest file, add total number of teams in the first line. From the second line on, add data as the format ```team_name problem_passed rank penalty``` with rank number ascending. Pay attention that only the winner team and teams you concerned that matters, you don't need to add all the teams. 
+1. Add teams into ```data/team.txt```
+2. For each contest, make a file named ```data/contest[*].txt```, which starts from 1. In the contest file, add total number of teams in the first line. From the second line on, add data as the format ```team_name problem_passed rank penalty``` with rank number ascending. Pay attention that only the winner team and teams you concerned that matters, you don't need to add all the teams. 
 3. run ```python3 rating.py``` in the terminal.
--- rating.py
+++ rating.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 from random import randint
 import csv
+import os
+
+fileDir = os.path.dirname(os.path.realpath('__file__'))
 
 K1 = 100
 
 team = {}
 
-with open('team.txt', 'r') as a:
+with open(os.path.join(fileDir, 'data/team.txt'), 'r') as a:
 	s = a.read().split()
 	for row in s:
 		team[row] = []
@@ -15,7 +18,7 @@ zjurating = open('rating.csv', 'w')
 	
 for i in range(1, 100):
 	try:
-		ftext = open('contest' + str(i) + '.txt', 'r').read().split('\n')
+		ftext = open(os.path.join(fileDir, 'data/contest' + str(i) + '.txt'), 'r').read().split('\n')
 		for key in team:
 			team[key].append('-')
 		cnt_team = int(ftext[0])
@@ -23,11 +26,8 @@ for i in range(1, 100):
 		for row in ftext[2:]:
 			lst = row.split()
 			nm = lst[0]
-			#print(lst)
 			cnt_p, rk, pen = map(int, lst[1:])
-			#print(cnt_p, rk, pen, cnt_prob_top, cnt_team)
 			rt = K1 * 1.0 * cnt_p / cnt_prob_top * (2 * cnt_team - 2) / (cnt_team + rk - 2)
-			#print("??")
 			if nm in team:
 				team[nm][-1] = rt
 	except:
@@ -36,26 +36,21 @@ for i in range(1, 100):
 	
 final_standing = []
 for x in team:
-	f = []
-	for y in team[x]:
-		if (y != '-'):
-			f.append(y)
+	f = list(filter(lambda x : x != '-', team[x]))
 	f.sort()
 	ans = 0
-	if (len(f) == 0):
+	if len(f) == 0:
 		ans = 0
 	else:
 		m = len(f)
 		m -= m // 4
-		for i in range(m):
-			ans += f[-i-1]
-		ans /= m
+		ans = sum(f[-m:]) / m
 	final_standing.append([-ans, x])
 	
 final_standing.sort()
 
 zjuhtml = open("rating.html", "w")
-temp1 = open("template.1", "r").read()
+temp1 = open(os.path.join(fileDir, "html/template.1"), "r").read()
 zjuhtml.write(temp1)
 
 cnt_contest = 0
@@ -67,27 +62,16 @@ dli_th = """</th><th class="stnd">"""
 dli_td = """</td><td class="stnd">"""
 zjuhtml.write('<tr><th class="stnd">' + (dli_th.join(['Place', 'Name'] + list(map(lambda x : "Contest " + str(x), range(1, cnt_contest + 1))) + ['Rating'])) + '</th><tr>\n')
 	
-Pac = 0
-for x in final_standing:
+for (pac, x) in enumerate(final_standing):
 	print(-x[0], x[1], team[x[1]])
 	zjurating.write(str(-x[0]) + ', ' + x[1] + ', ' + ', '.join(map(str, team[x[1]])) + '\n')
-	Pac += 1
-	sss = []
-	for y in team[x[1]]:
-		if (y == '-'):
-			sss.append('-')
-		else:
-			sss.append("%.3f" % y)
-	if True:
-		COL = ""
-		for i in range(6):
-			COL += hex(randint(0, 15))[2:]
-		zjuhtml.write('<tr><td class="stnd">' + (dli_td.join([str(Pac), "<font color=" + COL + ">" + x[1] + "</font>"] + sss + ["%.10f" % (-x[0])])) + '</td></tr>\n')
-	else:
-		zjuhtml.write('<tr><td class="stnd">' + (dli_td.join([str(Pac), x[1]] + sss + ["%.10f" % (-x[0])])) + '</td></tr>\n')
-	
+	sss = ['-' if y == '-' else ("%.3f" % y) for y in team[x[1]]]
+	COL = ""
+	for i in range(6):
+		COL += hex(randint(0, 15))[2:]
+	zjuhtml.write('<tr><td class="stnd">' + (dli_td.join([str(pac+1), "<font color=" + COL + ">" + x[1] + "</font>"] + sss + ["%.10f" % (-x[0])])) + '</td></tr>\n')
 
-temp2 = open("template.2", "r").read()
+temp2 = open(os.path.join(fileDir, "html/template.2"), "r").read()
 zjuhtml.write(temp2)
 zjurating.close()
 zjuhtml.close()
